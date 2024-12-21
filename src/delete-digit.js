180f94738c8c457141f4cbb27fc4d1366d6a6356
toryzb <tory.zb@gmail.com>
2024-12-21 14:28:09 +0300

    feat: solve some tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = String(n).split('');
+  let result = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    let newArr = arr.slice();
+    newArr.splice(i, 1);
+    result.push(Number(newArr.join('')));
+  }
+  return Math.max(...result);
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error('Invalid date!');
+  }
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date!');
+  }
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  if (day < 1 || day > 31 || month < 0 || month > 11 || year < 0) {
+    throw new Error('Invalid date!');
+  }
+  if (month === 0 || month === 1 || month === 11) {
+    return 'winter';
+  }
+  if (month >= 2 && month <= 4) {
+    return 'spring';
+  }
+  if (month >= 5 && month <= 7) {
+    return 'summer';
+  }
+    return 'autumn';
+  
 }
 
 module.exports = {
