087e1d5c5fc9eba9307908831fc73592e9ab2650
toryzb <tory.zb@gmail.com>
2024-12-23 20:40:18 +0300

    feat: solve almost all tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    let keyIndex = 0;
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i += 1) {
+      const msgChar = message[i];
+      const keyChar = key[keyIndex % key.length];
+      if (alphabet.indexOf(msgChar) === -1) {
+        result += msgChar;
+      } else {
+        const msgIndex = alphabet.indexOf(msgChar);
+        const keyIndexChar = alphabet.indexOf(keyChar);
+        const encryptedIndex = (msgIndex + keyIndexChar) % 26;
+        result += alphabet[encryptedIndex];
+        keyIndex += 1;
+      }
+    }
+
+    if (!this.direct) {
+      result = result.split('').reverse().join('');
+    }
+
+    return result;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    let keyIndex = 0;
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i += 1) {
+      const msgChar = message[i];
+      const keyChar = key[keyIndex % key.length];
+      if (alphabet.indexOf(msgChar) === -1) {
+        // Если символ не является буквой, добавляем его как есть
+        result += msgChar;
+      } else {
+        // Если символ является буквой
+        const msgIndex = alphabet.indexOf(msgChar);
+        const keyIndexChar = alphabet.indexOf(keyChar);
+        const decryptedIndex = (msgIndex - keyIndexChar + 26) % 26; // Добавляем 26, чтобы результат был положительным
+        result += alphabet[decryptedIndex];
+        keyIndex += 1; // Увеличиваем индекс ключа
+      }
+    }
+
+    // Если обратное шифрование, меняем местами символы
+    if (!this.direct) {
+      result = result.split('').reverse().join('');
+    }
+
+    return result;
   }
 }
 
